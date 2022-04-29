738ce123800d949f56ff46db57a89518408fb387
swj369451 <swj369451@163.com>
2022-04-29 14:43:24 +0800

    Initial commit

---
+++ src/main/java/com/example/object_relational_mapping/ObjectRelationalMappingApplication.java
@@ -0,0 +1,13 @@
+package com.example.object_relational_mapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectRelationalMappingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObjectRelationalMappingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/object_relational_mapping/ObjectRelationalMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.object_relational_mapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectRelationalMappingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
