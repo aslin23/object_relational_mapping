3924e5f17741248131825d560d563721a709334a
swj369451 <swj369451@163.com>
2022-04-29 15:56:23 +0800

    添加接口

---
+++ src/main/java/com/example/object_relational_mapping/DataStoreObject.java
@@ -0,0 +1,10 @@
+package com.example.object_relational_mapping;
+
+/**
+ * @program: object_relational_mapping
+ * @description数据存储对象
+ * @author: 苏敏
+ * @create: 2022-04-29 14:47
+ **/
+public interface DataStoreObject {
+}
